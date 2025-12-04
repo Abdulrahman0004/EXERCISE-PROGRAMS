78dae47734b531d95eb959f5b3ffd62b3b97fdcc
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2025-12-04 13:52:44 +0530

    Delete PYTHON  PROGRAMS/Factorial Program.py

--- PYTHON  PROGRAMS/Factorial Program.py
+++
@@ -1,7 +0,0 @@
-#4. Write a Python program to find a factorial of a given number
-
-num = int(input("Enter the number: "))
-fact = 1
-for i in range(num, 0, -1):
-    fact = fact * i
-print("The Factorial of", num, "is", fact)
