e95c7ba943dc4948f5ed3fcf0df5d87d5db17ae7
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2026-06-29 07:25:13 +0530

    Add files via upload

---
+++ PYTHON  PROGRAMS/Second largest element in array.py
@@ -0,0 +1,15 @@
+arr = [2, 5, 7, 10, 6, 3, 2, 11, 9, 8, 12]
+large = float('-inf')
+second_large = float('inf')
+
+for i in range(len(arr)):
+    if arr[i] > large:
+        second_large = large
+        large = arr[i]
+    elif arr[i] > second_large and arr[i] != large:
+        second_large = arr[i]
+
+if second_large == '-inf':
+    print(-1)
+else:
+    print(second_large)
