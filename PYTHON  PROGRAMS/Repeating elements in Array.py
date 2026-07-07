d026c9c3481be2b63441d764e433ad91f39ea612
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2026-07-07 06:25:45 +0530

    Add files via upload

---
+++ PYTHON  PROGRAMS/Repeating elements in Array.py
@@ -0,0 +1,17 @@
+def repeatingArray(arr):
+    if len(arr) == 0:
+        return None
+    result = {}
+
+    for i in arr:
+        if i not in result:
+            result[i] = 1
+        else:
+            result[i] += 1
+
+    for key in result:
+        if result[key] > 1:
+            print(key)
+
+arr = [1, 2, 3, 2, 3, 4, 5, 6, 7]
+repeatingArray(arr)
